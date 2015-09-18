350340a9a0ceadf542da63041f137cabbca90e7e
Sergio Fruto <fruto.sergio@gmail.com>
2015-09-18 00:50:54 -0300

    added js files for materialize + login styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@
     // ES6
     console.log([1, 2, 3].map((v) => v * v));
 }());
+
+$( document ).ready(function(){
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  console.log ("nav");
+})
