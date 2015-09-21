5eb7501d3c7c11cc12443dd7d549ed98139d3783
Sergio Fruto <fruto.sergio@gmail.com>
2015-09-20 23:57:48 -0300

    Modal logic working (only in mi historial de problemas/nuevo problema)

--- src/js/init.js
+++ src/js/init.js
@@ -3,6 +3,11 @@
 
     $('.button-collapse').sideNav();
     $('select').material_select();
+    $('.modal-trigger').leanModal();
+
+    $('.toggle-show').click(function() {
+        $( '#prob_more_info' ).toggleClass("hide");
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
