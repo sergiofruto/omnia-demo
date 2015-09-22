bfd8820f6e548ddf8522710b4e80c2e0e6d391cb
Sergio Fruto <fruto.sergio@gmail.com>
2015-09-22 09:50:13 -0300

    final details to nav and login

--- src/js/init.js
+++ src/js/init.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('select').material_select();
     $('.modal-trigger').leanModal();
+    $(".dropdown-button").dropdown();
 
     $('.toggle-show').click(function() {
         $( '#prob_more_info' ).toggleClass("hide");
